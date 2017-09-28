ce4a21ebb74db28ad808afed91922a01451484f1
oohyem <32355740+oohyem@users.noreply.github.com>
2017-09-28 17:39:33 +0900

    Delete sketch.js

--- sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-createCanvas(windowWidth, windowHeight); background(0,100,0);
-}
-function draw() {
-if (mouseIsPressed) {
-var size = random(10, 100);
-ellipse(mouseX, mouseY, size, size); }
-}
